047a0d62e49213100ff7aa0a25199f9cab3956d3
Dakhnovich Andrei <Dakhnovich Andrei>
2016-04-18 01:01:12 +0300

    Created MainActivity layout and class

---
+++ app/src/main/java/com/example/skogs/wifictrl/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.skogs.wifictrl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by skogs on 18.04.2016.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
