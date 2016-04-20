be26c9e54b3edbfed090cb8a21ee41e6bb7c0028
Dakhnovich Andrei <Dakhnovich Andrei>
2016-04-20 23:11:29 +0300

    Created NavigationView

--- app/src/main/java/com/example/skogs/wifictrl/MainActivity.java
+++ app/src/main/java/com/example/skogs/wifictrl/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.skogs.wifictrl;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by skogs on 18.04.2016.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem){
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
